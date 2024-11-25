4f22935c390acf17adf3c16b1769fe1fb10c21e8
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2024-11-25 21:12:03 +0900

    feat: TransactionDto 생성

---
+++ src/main/java/com/example/account/dto/TransactionDto.java
@@ -0,0 +1,35 @@
+package com.example.account.dto;
+
+import com.example.account.domain.Transcation;
+import com.example.account.type.TransactionResultType;
+import com.example.account.type.TransactionType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TransactionDto {
+    private String accountNumber;
+    private TransactionType transactionType;
+    private TransactionResultType transactionResultType;
+    private Long amount;
+    private Long balanceSnapshot;
+    private String transactionId;
+    private LocalDateTime transactionAt;
+
+    public static TransactionDto fromEntity(Transcation transaction) {
+        return TransactionDto.builder()
+                .accountNumber(transaction.getAccount().getAccountNumber())
+                .transactionType(transaction.getTransactionType())
+                .transactionResultType(transaction.getTransactionResultType())
+                .amount(transaction.getAmount())
+                .balanceSnapshot(transaction.getBalanceSnapshot())
+                .transactionId(transaction.getTransactionId())
+                .transactionAt(transaction.getTransactionAt())
+                .build();
+    }
+}
